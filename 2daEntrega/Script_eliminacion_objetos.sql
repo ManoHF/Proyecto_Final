afbfae24175d6dae3ca9899c5a7967bb909eb729
ManoHF <70402438+ManoHF@users.noreply.github.com>
2021-05-09 19:48:35 -0500

    Add files via upload

---
+++ 2daEntrega/Script_eliminacion_objetos.sql
@@ -0,0 +1,23 @@
+-- Script Drop tables --
+
+drop table hospital_structure_for_historicals;
+
+drop table covid_protocol;
+
+drop table patient_statistics;
+
+drop table staff;
+
+drop table person;
+
+drop table inventory ;
+
+drop table telephone;
+
+drop table contact;
+
+drop table users;
+
+drop table hospital;
+
+drop table interview;
