bdb235c9dc1117640c285987944fd0e85f741aad
ManoHF <70402438+ManoHF@users.noreply.github.com>
2021-05-17 21:18:47 -0500

    Create Script_eliminacion.sql

---
+++ 3raEntrega/Scripts_modificados/Script_eliminacion.sql
@@ -0,0 +1,23 @@
+-- Script Drop tables --
+
+drop table hospital_structure;
+
+drop table covid_protocol;
+
+drop table patient_statistics;
+
+drop table staff;
+
+drop table person;
+
+drop table inventory ;
+
+drop table telephone;
+
+drop table contact;
+
+drop table interview;
+
+drop table hospital;
+
+drop table users;
