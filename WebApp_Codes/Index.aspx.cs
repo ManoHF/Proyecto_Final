14f8e36775ad2006ee5b33c82db7d67add7508f8
ManoHF <70402438+ManoHF@users.noreply.github.com>
2021-05-21 22:31:30 -0500

    Update Index.aspx.cs

--- WebApp_Codes/Index.aspx.cs
+++ WebApp_Codes/Index.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -48,21 +48,11 @@ namespace Proyecto_BD
                 Conexion.llenarDDLProblemas(ddlM2);
                 Conexion.llenarDDLAcciones(ddlM3);
 
-                //Llenado de preguntas con rangos de números
-                Conexion.llenarDDLNumeros1(ddlNum19);
-                Conexion.llenarDDLNumeros1(ddlNum20);
-
-                //Llenado de preguntas con rango de números con opción 9 como "no sé"
-                Conexion.llenarDDLNumeros2(ddlNum21);
-                Conexion.llenarDDLNumeros2(ddlNum22);
-                Conexion.llenarDDLNumeros2(ddlNum23);
-                Conexion.llenarDDLNumeros2(ddlNum24);
-                Conexion.llenarDDLNumeros2(ddlNum25);
-                Conexion.llenarDDLNumeros2(ddlNum26);
-
                 //Lllenar dropdownlist de user id
                 Users u = new Users();
                 u.llenaDDLUsers(ddlUser);
+
+                
 
             }
         }
@@ -95,39 +85,180 @@ namespace Proyecto_BD
             if (id_hospital == 0)
                 id_hospital = h1.regresaID(ddHospital.SelectedValue);
 
-            Inventory i = new Inventory(id_hospital, Convert.ToInt16(ddlReserves1.SelectedValue), Convert.ToInt16(ddlReserves2.SelectedValue),
-                Convert.ToInt16(ddlReserves3.SelectedValue), Convert.ToInt16(ddlReserves4.SelectedValue), Convert.ToInt16(ddlReserves5.SelectedValue), Convert.ToInt16(ddlReserves6.SelectedValue),
-                Convert.ToInt16(ddlReserves7.SelectedValue), Convert.ToInt16(ddlReserves8.SelectedValue), Convert.ToInt16(ddlReserves9.SelectedValue), Convert.ToInt16(ddlReserves10.SelectedValue),
-                Convert.ToInt16(ddlReserves11.SelectedValue), Convert.ToInt16(ddlReserves12.SelectedValue));
-            int inventario = i.registraInventario();
+            if (id_hospital > 0 && ddlM1.SelectedIndex != 0 && ddlUser.SelectedIndex != 0)
+            {
+                lbUser.Text = ""; lbStatus.Text = ""; lbHos.Text = ""; lbRes.Text = "";
 
-            Staff s = new Staff(id_hospital, ddlNum19.SelectedValue , ddlNum20.SelectedValue);
-            int staff = s.registraStaff();
+                String reserves1, reserves2, reserves3, reserves4, reserves5, reserves6, reserves7,
+                reserves8, reserves9, reserves10, reserves11, reserves12;
 
-            bool screen = false, campaign = false, ability = false, track = false;
-            if (ddlYN13.SelectedValue == "YES")
-                screen = true;
-            if (ddlYN14.SelectedValue == "YES")
-                campaign = true;
-            if (ddlYN15.SelectedValue == "YES")
-                ability = true;
-            if (ddlYN17.SelectedValue == "YES")
-                track = true;
-            Covid_protocol cp = new Covid_protocol(id_hospital, screen, campaign, ability, ddlRecursos16.SelectedValue, track, ddlRastreo18.SelectedValue);
-            int protocol = cp.registraProtocol();
+                if (ddlReserves1.SelectedValue == "[Select an answer]")
+                    reserves1 = "null";
+                else
+                    reserves1 = ddlReserves1.SelectedValue;
+                if (ddlReserves2.SelectedValue == "[Select an answer]")
+                    reserves2 = "null";
+                else
+                    reserves2 = ddlReserves2.SelectedValue;
+                if (ddlReserves3.SelectedValue == "[Select an answer]")
+                    reserves3 = "null";
+                else
+                    reserves3 = ddlReserves3.SelectedValue;
+                if (ddlReserves4.SelectedValue == "[Select an answer]")
+                    reserves4 = "null";
+                else
+                    reserves4 = ddlReserves4.SelectedValue;
+                if (ddlReserves5.SelectedValue == "[Select an answer]")
+                    reserves5 = "null";
+                else
+                    reserves5 = ddlReserves5.SelectedValue;
+                if (ddlReserves6.SelectedValue == "[Select an answer]")
+                    reserves6 = "null";
+                else
+                    reserves6 = ddlReserves6.SelectedValue;
+                if (ddlReserves7.SelectedValue == "[Select an answer]")
+                    reserves7 = "null";
+                else
+                    reserves7 = ddlReserves7.SelectedValue;
+                if (ddlReserves8.SelectedValue == "[Select an answer]")
+                    reserves8 = "null";
+                else
+                    reserves8 = ddlReserves8.SelectedValue;
+                if (ddlReserves9.SelectedValue == "[Select an answer]")
+                    reserves9 = "null";
+                else
+                    reserves9 = ddlReserves9.SelectedValue;
+                if (ddlReserves10.SelectedValue == "[Select an answer]")
+                    reserves10 = "null";
+                else
+                    reserves10 = ddlReserves10.SelectedValue;
+                if (ddlReserves11.SelectedValue == "[Select an answer]")
+                    reserves11 = "null";
+                else
+                    reserves11 = ddlReserves11.SelectedValue;
+                if (ddlReserves12.SelectedValue == "[Select an answer]")
+                    reserves12 = "null";
+                else
+                    reserves12 = ddlReserves12.SelectedValue;
 
-            Patient_statistics ps = new Patient_statistics(id_hospital, ddlNum21.SelectedValue, ddlNum22.SelectedValue, ddlNum23.SelectedValue, 
-                ddlNum24.SelectedValue, ddlNum25.SelectedValue, ddlNum26.SelectedValue);
-            int stats = ps.registraPatient_statistics();
 
-            Interview it = new Interview(id_hospital, Convert.ToInt16(ddlUser.SelectedValue), Convert.ToInt16(tbMOPH.Text),
-                ddlM1.SelectedValue, ddlM2.SelectedValue, ddlM3.SelectedValue);
-            it.registraEntrevista();
+                Inventory i = new Inventory(id_hospital, reserves1, reserves2, reserves3, reserves4, reserves5,
+                    reserves6, reserves7, reserves8, reserves9, reserves10, reserves11, reserves12);
+                int inventario = i.registraInventario();
 
-            lbRegistro.Text = id_hospital + " " + inventario + " " + stats + " " + protocol + " " + staff;
 
-            Hospital_structure hs = new Hospital_structure(id_hospital, inventario, stats, protocol, staff);
-            hs.registraStructure();
+                String screen = "false", campaign = "false", ability = "false", track = "false";
+                string resources, tracking;
+
+                if (ddlYN13.SelectedValue == "YES")
+                    screen = "true";
+                if (ddlYN14.SelectedValue == "YES")
+                    campaign = "true";
+                if (ddlYN15.SelectedValue == "YES")
+                    ability = "true";
+                if (ddlYN17.SelectedValue == "YES")
+                    track = "true";
+                if (ddlYN13.SelectedValue == "[Select an answer]")
+                    screen = "null";
+                if (ddlYN14.SelectedValue == "[Select an answer]")
+                    campaign = "null";
+                if (ddlYN15.SelectedValue == "[Select an answer]")
+                    ability = "null";
+                if (ddlYN17.SelectedValue == "[Select an answer]")
+                    track = "null";
+                if (ddlRecursos16.SelectedValue == "[Select an answer]")
+                    resources = "null";
+                else
+                    resources = "'" + ddlRecursos16.SelectedValue + "'";
+                if (ddlRastreo18.SelectedValue == "[Select an answer]")
+                    tracking = "null";
+                else
+                    tracking = "'" + ddlRastreo18.SelectedValue + "'";
+
+                Covid_protocol cp = new Covid_protocol(id_hospital, screen, campaign, ability, resources, track, tracking);
+                int protocol = cp.registraProtocol();
+
+                String numDoctors, numParamedics;
+
+                if (tbDoctors.Text == "")
+                    numDoctors = "null";
+                else
+                    numDoctors = tbDoctors.Text;
+                if (tbParamedics.Text == "")
+                    numParamedics = "null";
+                else
+                    numParamedics = tbParamedics.Text;
+
+                Staff s = new Staff(id_hospital, numDoctors, numParamedics);
+                int staff = s.registraStaff();
+
+                //Recoleccion de datos para su posterior insercion en la tabla patient statistics
+                String q21, q22, q23, q24, q25, q26;
+
+                if (tbQ21.Text == "")
+                    q21 = "null";
+                else
+                    q21 = tbQ21.Text;
+                if (tbQ22.Text == "")
+                    q22 = "null";
+                else
+                    q22 = tbQ22.Text;
+                if (tbQ23.Text == "")
+                    q23 = "null";
+                else
+                    q23 = tbQ23.Text;
+                if (tbQ24.Text == "")
+                    q24 = "null";
+                else
+                    q24 = tbQ24.Text;
+                if (tbQ25.Text == "")
+                    q25 = "null";
+                else
+                    q25 = tbQ25.Text;
+                if (tbQ26.Text == "")
+                    q26 = "null";
+                else
+                    q26 = tbQ26.Text;
+
+                Patient_statistics ps = new Patient_statistics(id_hospital, q21, q22, q23, q24, q25, q26);
+                int stats = ps.registraPatient_statistics();
+
+                String problem, action, moph;
+
+                if (tbMOPH.Text == "")
+                    moph = "null";
+                else
+                    moph = tbMOPH.Text;
+                if (ddlM2.SelectedValue.Equals("[Select an answer]"))
+                    problem = "null";
+                else
+                    problem = "'" + ddlM2.SelectedValue + "'";
+                if (ddlM3.SelectedValue.Equals("[Select an answer]"))
+                    action = "null";
+                else
+                    action = "'" + ddlM3.SelectedValue + "'";
+
+
+                Interview it = new Interview(id_hospital, Convert.ToInt16(ddlUser.SelectedValue), moph,
+                    ddlM1.SelectedValue, problem, action);
+                it.registraEntrevista();
+
+                Hospital_structure hs = new Hospital_structure(id_hospital, inventario, stats, protocol, staff);
+                hs.registraStructure();
+
+                lbRes.Text = "Registro exitoso";
+            }
+            else
+            {
+                lbRes.Text = "Faltan alguno de los datos obligatorios: hospital, status, o id usuario";
+                if (id_hospital <= 0)
+                    lbHos.Text = "* Obligatorio";
+                if (ddlM1.SelectedIndex == 0)
+                    lbStatus.Text = "* Obligatorio";
+                if (ddlUser.SelectedIndex == 0)
+                    lbUser.Text = "* Obligatorio";
+            }
+            
         }
 
         protected void btUpdate_Click(object sender, EventArgs e)
