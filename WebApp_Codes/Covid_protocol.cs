0ddc8d21bcd2d47307352143ed4a365f6f7e6dbf
ManoHF <70402438+ManoHF@users.noreply.github.com>
2021-05-21 22:28:40 -0500

    Update Covid_protocol.cs

--- WebApp_Codes/Covid_protocol.cs
+++ WebApp_Codes/Covid_protocol.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,11 +10,11 @@ namespace Proyecto_BD
     public class Covid_protocol
     {
 		private int id_hospital;
-		private bool screen_covid_patients;
-		private bool preventions_campaigns;
-		private bool current_ability_for_tests;
+		private string screen_covid_patients;
+		private string preventions_campaigns;
+		private string current_ability_for_tests;
 		private string resources_for_covid;
-		private bool track_regularly_cases;
+		private string track_regularly_cases;
 		private string report_covid_result_to_moph_days;
 		Covid_protocol cp;
 
@@ -22,7 +22,7 @@ namespace Proyecto_BD
         {
         }
 
-        public Covid_protocol(int id_hospital, bool screen_covid_patients, bool preventions_campaigns, bool current_ability_for_tests, string resources_for_covid, bool track_regularly_cases, string report_covid_result_to_moph_days)
+        public Covid_protocol(int id_hospital, string screen_covid_patients, string preventions_campaigns, string current_ability_for_tests, string resources_for_covid, string track_regularly_cases, string report_covid_result_to_moph_days)
         {
             this.id_hospital = id_hospital;
             this.screen_covid_patients = screen_covid_patients;
@@ -39,9 +39,9 @@ namespace Proyecto_BD
 			NpgsqlConnection con;
 			NpgsqlDataReader rd;
 			int res = -1;
-			String q = "Select nextval(pg_get_serial_sequence('voxmapp.covid_protocol', 'id_covid_protocol'))";
+			String q = "Select currval(pg_get_serial_sequence('voxmapp.covid_protocol', 'id_covid_protocol'))";
 			String query = "insert into voxmapp.covid_protocol (id_hospital, screen_covid_patients, preventions_campaigns, currently_ability_for_tests, resources_for_covid, track_regularly_cases, report_covid_result_to_moph_days) values (" + id_hospital + ", " +
-				screen_covid_patients + ", " + preventions_campaigns + ", " + current_ability_for_tests + ", '" + resources_for_covid + "', " + track_regularly_cases + ", '" + report_covid_result_to_moph_days + "')";
+				screen_covid_patients + ", " + preventions_campaigns + ", " + current_ability_for_tests + ", " + resources_for_covid + ", " + track_regularly_cases + ", " + report_covid_result_to_moph_days + ")";
 			try
 			{
 				con = Conexion.agregarConexion();
@@ -51,7 +51,7 @@ namespace Proyecto_BD
 				rd = cmd2.ExecuteReader();
 				if (rd.Read())
 				{
-					res = rd.GetInt16(0)-1;
+					res = rd.GetInt16(0);
 				}
 				con.Close();
 				return res;
