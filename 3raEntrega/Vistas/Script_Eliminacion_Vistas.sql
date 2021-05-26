af905d895a0dee19276ac2f890a0a1e7096f4489
ManoHF <70402438+ManoHF@users.noreply.github.com>
2021-05-25 21:33:20 -0500

    Add files via upload

---
+++ 3raEntrega/Vistas/Script_Eliminacion_Vistas.sql
@@ -0,0 +1,31 @@
+-- Eliminacion de vistas
+
+-- Vista 1
+drop view needs_by_country;
+
+-- Vista 2
+drop view needs_by_province;
+
+-- Vista 3
+drop view needs_by_hospital;
+
+-- Vista 4
+drop view hosp_graves;
+
+-- Vista 5
+drop view protocols_percentage_country;
+
+-- Vista 6
+drop view staff_by_country;
+
+-- Vista 7
+drop view staff_by_province;
+
+-- Vista 8
+drop view covid_stats;
+
+-- Vista 9
+drop view deaths_stats_country;
+
+-- Vista 10
+drop view deaths_stats_by_province;
---
+++ 3raEntrega/Vistas/Script_Obtencion_Vistas.sql
@@ -0,0 +1,31 @@
+-- Obtención de la informacion contenida en las vistas
+
+-- Vista 1
+select * from needs_by_country;
+
+-- Vista 2
+select * from needs_by_province;
+
+-- Vista 3
+select * from needs_by_hospital;
+
+-- Vista 4
+select * from hosp_graves;
+
+-- Vista 5
+select * from protocols_percentage_country;
+
+-- Vista 6
+select * from staff_by_country;
+
+-- Vista 7
+select * from staff_by_province;
+
+-- Vista 8
+select * from covid_stats;
+
+-- Vista 9
+select * from deaths_stats_country;
+
+-- Vista 10
+select * from deaths_stats_by_province;
